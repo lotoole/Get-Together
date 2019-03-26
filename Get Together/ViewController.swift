d2bd6bd6fea26d6e483ec93191fd53715a7294f6
Liam T O'Toole <lotoole@uvm.edu>
2019-03-26 10:44:09 -0400

    added base mapbox map

--- Get Together/ViewController.swift
+++ Get Together/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Mapbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
+        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        view.addSubview(mapView)
+
     }
 
 
