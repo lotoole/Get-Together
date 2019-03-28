da39e5f73f651949b03ad83d63d7c23bb3daa470
Kyle Austin Michel <kyle.michel@uvm.edu>
2019-03-28 11:20:42 -0400

    Navigation to view controller

--- Get Together/ViewController.swift
+++ Get Together/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
-        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
-        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
-        view.addSubview(mapView)
+//        let url = URL(string: "mapbox://styles/mapbox/streets-v11")
+//        let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+//        view.addSubview(mapView)
 
     }
 
