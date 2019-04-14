f3eb767b9d7ce07db6a53922c629d30f48e6579d
Liam T O'Toole <lotoole@uvm.edu>
2019-04-14 16:12:47 -0400

    we have access to user location noew

--- Get Together/MapViewController.swift
+++ Get Together/MapViewController.swift
@@ -11,26 +11,27 @@ import Mapbox
 
 class MapViewController: UIViewController, MGLMapViewDelegate {
     
+    var location: CLLocationCoordinate2D!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let url = URL(string: "mapbox://styles/mapbox/streets-v11")
         let mapView = MGLMapView(frame: view.bounds, styleURL: url)
+        
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        mapView.setCenter(CLLocationCoordinate2D(latitude: 59.31, longitude: 18.06), zoomLevel: 9, animated: false)
+        
         mapView.tintColor = .darkGray
-        
-        //        Get User Location
-        
-        
-        //        set bounds based on user location
-        //        let bounds = MGLCoordinateBounds(
-        //            sw: CLLocationCoordinate2D(latitude: 43.7115, longitude: 10.3725),
-        //            ne: CLLocationCoordinate2D(latitude: 43.7318, longitude: 10.4222))
-        //        mapView.setVisibleCoordinateBounds(bounds, animated: false)
-        
+        // Set the map view‘s delegate property.
+        mapView.delegate = self
+        // Enable heading tracking mode so that the arrow will appear.
+        mapView.userTrackingMode = .followWithHeading
+        // Enable the permanent heading indicator, which will appear when the tracking mode is not `.followWithHeading`.
+        mapView.showsUserHeadingIndicator = true
+        mapView.showsUserLocation = true
         //add map
         view.addSubview(mapView)
+        
         //add button for adding events
         let button = UIButton(frame: CGRect(x: 300, y: 675, width: 100, height: 50))
         button.backgroundColor = .green
@@ -38,15 +39,16 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         view.addSubview(button)
         
-        // Set the map view‘s delegate property.
-        mapView.delegate = self
-        
     }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        mapView.setCenter((mapView.userLocation?.coordinate)!, animated: false)
+        location = mapView.userLocation?.coordinate
+    }
+    
     //call the add event function when button clicked
     @objc func buttonAction(sender: UIButton!) {
         print("Button tapped")
         self.performSegue(withIdentifier: "AddEventSegue", sender: self)
     }
-
-    
 }
