bf1c9e41d55dad719999f27aaf139c84e0aa43f0
xt806633519 <806633519@qq.com>
2019-04-23 11:08:38 -0400

    pods and firebase

--- Get Together/ViewController.swift
+++ Get Together/ViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 import Mapbox
+import Firebase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Email: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+//        Email.delegate = self
+//        Password.delegate = self
+//        Email.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+//        Password.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+//        
+    }
+    
+    @IBAction func user(_ sender: Any) {
+        print (Email.text!)
+    }
+  
+
+
+    
+    @IBAction func password(_ sender: Any) {
+        print (Password.text!)
 
     }
+    
+    @IBAction func click(_ sender: UIButton) {
+        var email = "tao@gmail.com"
+        var pass = "tao123"
+        
+        Auth.auth().signIn(withEmail: email, password: pass) { [weak self] user, error in
+            print(user!)
+            guard let strongSelf = self else { return }
+            
+            // ...
+            
+        }
+        print(Email.text!)
+        print(Password.text!)
 
-
+    }
+    
 }
 
