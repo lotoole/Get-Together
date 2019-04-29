274aec7e48c471dc8993d2d3b16eb0d93791b4a0
Liam T O'Toole <lotoole@uvm.edu>
2019-04-29 16:10:54 -0400

    added outlinte for single event scene and controller

---
+++ Get Together/SingleEventController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleEventController.swift
+//  Get Together
+//
+//  Created by Liam O'Toole on 4/29/19.
+//  Copyright © 2019 Liam O'Toole. All rights reserved.
+//
+
+import UIKit
+
+class SingleEventController: UIViewController {
+    @IBOutlet weak var TitleOutlet: UITextField!
+    @IBOutlet weak var TimeOutlet: UITextField!
+    @IBOutlet weak var AddressOutlet: UITextField!
+    @IBOutlet weak var CreatedByOutlet: UITextView!
+    
+    
+    override func viewDidLoad() {
+        
+        
+        
+    }
+    
+}
