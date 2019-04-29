75ffd7d9e6b2b8fb1fb93b6abbcab19c583d16d6
Gabriel Aboy <gabeaboy@gmail.com>
2019-04-29 12:48:39 -0700

    accessing json

--- Get Together/MapViewController.swift
+++ Get Together/MapViewController.swift
@@ -8,18 +8,59 @@
 import UIKit
 import Mapbox
 import MapboxGeocoder
+import Firebase
 class MapViewController: UIViewController, MGLMapViewDelegate {
     var mapView: MGLMapView!
     var location: CLLocationCoordinate2D!
     var mapConstraints: MGLCoordinateBounds!
     var coordinatesNewEvent:CLLocationCoordinate2D!
-    
+    var fakeEvents : Array<Event> = []
     override func viewDidLoad() {
         super.viewDidLoad()
         generateMapView()
         addCreateEventButton()
-        renderMapEvents(events : addFakeEvents())
+        getAllEvents()
+        addFakeEvents()
+        for item in self.fakeEvents{
+            print("Fake items ",item)
+        }
+        renderMapEvents(events : self.fakeEvents)
     
+    }
+    func getAllEvents(){
+        print("Getting all events")
+        var ref: DatabaseReference!
+        ref = Database.database().reference()
+        let userID = Auth.auth().currentUser?.uid
+        print("USER ID HERE!",userID)
+        ref.child("Events").observeSingleEvent(of: .value, with:{
+            (snapshot) in
+            let valueDict = snapshot.value as? NSDictionary
+
+            for(key, val) in valueDict! {
+                let event: NSObject = valueDict![key] as! NSObject
+                let title: String = event.value(forKey:"title")! as! String
+                let longitude: Double = event.value(forKey:"longitude")! as! Double
+                let latitude: Double = event.value(forKey:"latitude")! as! Double
+                let address: String = event.value(forKey:"address")! as! String
+                let createdBy: String = event.value(forKey:"createdBy")! as! String
+//                print(x)
+                print(latitude)
+                var eventDTO = Event(
+                 id: "12knd2",
+                 title: title,
+                 longitude:  longitude,
+                 latitude: latitude,
+                 address: address,
+                 createdBy: createdBy
+                )
+                print("Event working",eventDTO.latitude)
+                 self.fakeEvents.append(eventDTO)
+            }
+            }){
+            (error) in
+            print(error.localizedDescription)
+        }
     }
     func addCreateEventButton(){
         //add button for adding events
@@ -47,8 +88,7 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         mapView.showsUserLocation = true
     }
     //TODO: REMOVE THIS ONCE WE CAN POPULATE FROM DB
-    func addFakeEvents() -> Array<Event>{
-        var fakeEvents : Array<Event> = []
+    func addFakeEvents() -> Void{
         //fake event 1
         var fakeEvent1 = Event(id: "12knd2", title: "Funeral for my GPA", longitude:  -73.198474, latitude: 44.480138, address: "Perkins Hall, Burlington, VT 05405", createdBy: "Cam")
         //fake event 2
@@ -56,11 +96,11 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         //fake event 3
          var fakeEvent3 = Event(id: "5h2jh5", title: "Tao's Dinner", longitude: -73.199692, latitude: 44.480197, address: "University Pl, Burlington, VT 05405", createdBy: "Cam")
         
-        fakeEvents.append(fakeEvent1)
-        fakeEvents.append(fakeEvent2)
-        fakeEvents.append(fakeEvent3)
+        self.fakeEvents.append(fakeEvent1)
+        self.fakeEvents.append(fakeEvent2)
+        self.fakeEvents.append(fakeEvent3)
 
-        return fakeEvents
+//        return fakeEvents
     }
     
     func renderMapEvents(events: Array<Event>){
